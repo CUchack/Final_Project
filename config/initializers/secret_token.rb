795a59bab71c37bb19407a2c390bb0cf71975522
Chris Hack <christopher.hack@colorado.edu>
2014-03-26 23:08:31 -0600

    new blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecTut::Application.config.secret_token = '0c44e58f4616e5af21059aa367ca2a901194c4742246e4312afdce59be88f6e6793ae264617d794f5616c77acf9172a3d33815ebe4df1ff69c4c327814a54bc8'
