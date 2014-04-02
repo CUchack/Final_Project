ec008e705cbe3956bf1de3d81efea86fa335bd9f
Chris Hack <christopher.hack@colorado.edu>
2014-04-02 16:08:40 -0600

    changed layout to make basic FAKEBOOK template

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Static pages" do
 
   subject { page }
 
-  describe "Home page" do
+  describe "Login page" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
