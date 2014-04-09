7998c1490ee911a4b4f671e70f4fdd00940342d5
alji2106 <alix.jimenez@live.com>
2014-04-09 16:12:15 -0600

    More Login work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
- require 'user.rb'
+# require 'user.rb'
   def new
   render 'login'
   end
 
   def create
    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if (user && user.authenticate(params[:session][:password]))
       sign_in user
       redirect_to user
       # Sign the user in and redirect to the user's show page.
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
       uniqueness: { case_sensitive: false }
-  #has_secure_password
+  has_secure_password
   def create(name, email, password)
      :name = name
      :email = email
