6759a20a170d21a354d8246cbfc55ccbe8782c0a
CUchack <Christopher.Hack@colorado.edu>
2014-04-09 16:22:33 -0600

    made routes.rb look nicer

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ FinalProject::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup',  to: 'users#signup',            via: 'get'
+  match '/signup',  to: 'users#signup',         via: 'get'
  
  
   # The priority is based upon order of creation:
